faaa92dca0aa5800dd8c1993693d65ba3e6cc685
dahiya-04 <da24c020@smail.iitm.ac.in>
2025-11-24 11:11:39 +0530

    Delete run_pipeline.sh

--- run_pipeline.sh
+++
@@ -1,7 +0,0 @@
-
-
-python src/generate_data.py
-python src/train.py --model_name distilbert-base-uncased --train data/train.jsonl --dev data/dev.jsonl --out_dir out
-python src/predict.py --model_dir out --input data/dev.jsonl --output out/dev_pred.json
-python src/eval_span_f1.py --gold data/dev.jsonl --pred out/dev_pred.json
-python src/measure_latency.py --model_dir out --runs 50
